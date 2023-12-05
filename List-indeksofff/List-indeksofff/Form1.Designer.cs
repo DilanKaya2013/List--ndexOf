53b54770eecbac3cb852d4c9b4b96a40681ba860
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-12-05 13:00:22 +0300

    Add files via upload

---
+++ List-indeksofff/List-indeksofff/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace List_indeksofff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(196, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 433);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ List-indeksofff/List-indeksofff/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_indeksofff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> personelListe = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aranan = textBox1.Text;
+            int sira = personelListe.IndexOf(aranan);
+
+
+            if (sira >= 0)
+            {
+                listBox1.SelectedIndex = sira;
+                MessageBox.Show("Aradığınız personel " + (sira + 1) + ". sırada.");
+            }
+
+            else
+            {
+                MessageBox.Show("Personel bulunamadı.");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            personelListe.Add("Ahmet");
+            personelListe.Add("Burak");
+            personelListe.Add("Ayşe");
+            personelListe.Add("Beyza");
+            personelListe.Add("Aliye");
+            personelListe.Add("Kemal");
+            personelListe.Add("Ruziye");
+            personelListe.Add("Handan");
+            personelListe.Add("Muzaffer");
+
+            listBox1.DataSource = personelListe.ToList();
+        }
+    }
+}
